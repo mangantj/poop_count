28374cf52b260c718f09389d89bb4e4f78e45f52
Tyler Mangan <mangantj@gmail.com>
2024-05-03 19:08:29 -0500

    style poop index

--- app/helpers/poops_helper.rb
+++ app/helpers/poops_helper.rb
@@ -1,2 +1,5 @@
 module PoopsHelper
+  def display_poop_time(poop)
+    [poop.time.strftime("%A, %d %b %Y"), "@", poop.time.strftime("%I:%M %p")].join(" ")
+  end
 end
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,3 +1,7 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application", preload: true
+# pin "flowbite", to: "https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.turbo.min.js"
+pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
+pin "flowbite", to: "https://ga.jspm.io/npm:flowbite@2.3.0/lib/esm/index.js"
+pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.8/lib/index.js"
