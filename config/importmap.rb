3563e7aa6df0e628c8156fb7e402087626fab47c
Tyler Mangan <mangantj@gmail.com>
2024-05-08 18:55:31 -0500

    switch to turbo flowbite

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,9 +1,9 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application", preload: true
-# pin "flowbite", to: "https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.turbo.min.js"
+pin "flowbite", to: "https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.turbo.min.js"
 pin "@hotwired/turbo-rails", to: "https://ga.jspm.io/npm:@hotwired/turbo-rails@7.3.0/app/javascript/turbo/index.js"
-pin "flowbite", to: "https://ga.jspm.io/npm:flowbite@2.3.0/lib/esm/index.js"
+# pin "flowbite", to: "https://ga.jspm.io/npm:flowbite@2.3.0/lib/esm/index.js"
 pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.8/lib/index.js"
 pin "@hotwired/stimulus", to: "https://ga.jspm.io/npm:@hotwired/stimulus@3.2.2/dist/stimulus.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
