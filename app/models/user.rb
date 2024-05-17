3797f1ac74388963e53fc897c9481a4fd3075bf4
Tyler Mangan <mangantj@gmail.com>
2024-05-17 16:53:22 -0500

    list current users for league add fist and last name for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,8 @@ class User < ApplicationRecord
   has_many :poops
   has_many :league_users
   has_many :leagues, through: :league_users
+
+  def name
+    [first_name, last_name].join(" ")
+  end
 end
